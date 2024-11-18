efccddd42aec680f974b5e260028b7ae4d6c2836
Salutah <salutahmoha@gmail.com>
2024-11-18 16:33:21 +0300

    Create user with zustand in useStore.js

--- server/src/store/useStore.js
+++ server/src/store/useStore.js
@@ -1,25 +1,25 @@
-// import { create } from 'zustand';
-// import { devtools, persist } from 'zustand/middleware';
+import { create } from 'zustand';
+import { devtools, persist } from 'zustand/middleware';
 
-// function userStore(set) {
-//   return {
-//     user: null,
-//     role: null,
+function userStore(set) {
+  return {
+    user: null,
+    role: null,
 
-//     setUser: function (userInformationObj) {
-//       set((state) => {
-//         return { user: userInformationObj, role: userInformationObj.role };
-//       });
-//     },
+    setUser: function (userInformationObj) {
+      set((state) => {
+        return { user: userInformationObj, role: userInformationObj.role };
+      });
+    },
 
-//     logoutUser: function () {
-//       set((state) => {
-//         return { user: null, role: null };
-//       });
-//     },
-//   };
-// }
+    logoutUser: function () {
+      set((state) => {
+        return { user: null, role: null };
+      });
+    },
+  };
+}
 
-// const useUserStore = create(devtools(persist(userStore, { name: 'auth-storage' })));
+const useUserStore = create(devtools(persist(userStore, { name: 'auth-storage' })));
 
-// export default useUserStore;
+export default useUserStore;
