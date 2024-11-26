561e2ceec2048f3ae4bd66d2db577d5e0f1c7884
Salutah <salutahmoha@gmail.com>
2024-11-26 12:20:35 +0300

    Connecting the update personal information with backend

--- client/src/components/ProfileInformation/UpdatePersonalInformation.jsx
+++ client/src/components/ProfileInformation/UpdatePersonalInformation.jsx
@@ -7,64 +7,64 @@ import useUserStore from '../../../../server/src/store/useStore';
 import { Container, FormContainer, Form, Title, Label, Input, UpdatePersonalInfoButton } from "../StyledComponents/UpdatePersonalInformation";
 
 function UpdatePersonalInformation() {
-    // const [firstName, setFirstname] = useState('');
-    // const [lastName, setLastname] = useState('');
-    // const [emailAddress, setEmail] = useState('');
-    // const [username, setUsername] = useState('');
+    const [firstName, setFirstname] = useState('');
+    const [lastName, setLastname] = useState('');
+    const [emailAddress, setEmail] = useState('');
+    const [username, setUsername] = useState('');
 
-    // const user = useUserStore((state) => state.user);
-    // const setUser = useUserStore((state) => state.setUser);
+    const user = useUserStore((state) => state.user);
+    const setUser = useUserStore((state) => state.setUser);
 
-    // useEffect(() => {
-    //     if (user && user.user) {
-    //         console.log("User data in useEffect:", user.user);
-    //         setFirstname(user.user.firstName);
-    //         setLastname(user.user.lastName);
-    //         setEmail(user.user.emailAddress);
-    //         setUsername(user.user.username);
-    //     }
-    // }, [user]);
+    useEffect(() => {
+        if (user && user.user) {
+            console.log("User data in useEffect:", user.user);
+            setFirstname(user.user.firstName);
+            setLastname(user.user.lastName);
+            setEmail(user.user.emailAddress);
+            setUsername(user.user.username);
+        }
+    }, [user]);
     
 
-    // const { mutate, isLoading } = useMutation({
-    //     mutationFn: async function (updatedUser) {
-    //         const response = await fetch(`${apiBase}/users`, {
-    //             method: 'PUT',
-    //             headers: { 'Content-Type': 'application/json' },
-    //             body: JSON.stringify(updatedUser),
-    //             credentials: 'include'
-    //         });
+    const { mutate, isLoading } = useMutation({
+        mutationFn: async function (updatedUser) {
+            const response = await fetch(`${apiBase}/users`, {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(updatedUser),
+                credentials: 'include'
+            });
 
-    //         if (!response.ok) {
-    //             const error = await response.text();
-    //             throw new Error(error);
-    //         }
+            if (!response.ok) {
+                const error = await response.text();
+                throw new Error(error);
+            }
 
-    //         const data = await response.json();
-    //         return data;
-    //     },
-    //     onSuccess: (data) => {
-    //         setUser(data);
-    //         toast("Personal Information Updated", { theme: "toast-success", duration: 2000 });
-    //     },
-    //     onError: (error) => {
-    //         toast(error.message, { theme: "toast-error", duration: 2000 });
-    //     }
-    // });
+            const data = await response.json();
+            return data;
+        },
+        onSuccess: (data) => {
+            setUser(data);
+            toast("Personal Information Updated", { theme: "toast-success", duration: 2000 });
+        },
+        onError: (error) => {
+            toast(error.message, { theme: "toast-error", duration: 2000 });
+        }
+    });
 
-    // function handleUpdatePersonalInformation(e) {
-    //     e.preventDefault();
-    //     if (!firstName || !lastName || !emailAddress || !username) {
-    //         toast("All fields are required", { theme: "toast-error", duration: 2000 });
-    //         return;
-    //     }
+    function handleUpdatePersonalInformation(e) {
+        e.preventDefault();
+        if (!firstName || !lastName || !emailAddress || !username) {
+            toast("All fields are required", { theme: "toast-error", duration: 2000 });
+            return;
+        }
 
-    //     mutate({ firstName, lastName, emailAddress, username });
-    // }
+        mutate({ firstName, lastName, emailAddress, username });
+    }
 
-    // if (!user) {
-    //     return <div>Loading...</div>;  // Optional loading state while user data is being fetched
-    // }
+    if (!user) {
+        return <div>Loading...</div>;  // Optional loading state while user data is being fetched
+    }
 
     return (
         <Container>
