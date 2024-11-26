480a73a1ff65b03ab99400debfa315201584f4eb
Salutah <salutahmoha@gmail.com>
2024-11-26 12:06:57 +0300

    Implement a get user profile route

--- server/src/controllers/reports.controllers.js
+++ server/src/controllers/reports.controllers.js
@@ -100,26 +100,26 @@ export async function createProfile(req, res) {
     }
 }
 
-// // Getting profile for  logged in user
-// export  async function  getUserProfie(req, res) {
-//     try {
-//         const userId = req.userId;  // Assuming verifyToken sets req.userId
+// Getting profile for  logged in user
+export  async function  getUserProfie(req, res) {
+    try {
+        const userId = req.userId;  // Assuming verifyToken sets req.userId
 
-//         // Fetch profile based on the authenticated user's ID
-//         const profile = await prisma.profile.findUnique({
-//             where: { userId },
-//         });
+        // Fetch profile based on the authenticated user's ID
+        const profile = await prisma.profile.findUnique({
+            where: { userId },
+        });
 
-//         if (!profile) {
-//             return res.status(404).json({ message: "Profile not found" });
-//         }
+        if (!profile) {
+            return res.status(404).json({ message: "Profile not found" });
+        }
 
-//         res.status(200).json(profile);
-//     } catch (error) {
-//         console.error("Error fetching profile:", error);
-//         res.status(500).json({ message: "An error occurred while fetching the profile." });
-//     }
-// };
+        res.status(200).json(profile);
+    } catch (error) {
+        console.error("Error fetching profile:", error);
+        res.status(500).json({ message: "An error occurred while fetching the profile." });
+    }
+};
 
 // //update profile 
 // export async function updateProfile(req, res) {
